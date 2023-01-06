3e7ace1f50c889a9af614ec1a031318a0292482a
Arkadiusz Agaciak <arek.agaciak@gmail.com>
2023-01-06 23:43:28 +0100

    cypress tests added

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,9 @@
+describe('Test on local srv', () => {
+  it('passes', () => {
+    cy.visit('http://localhost/')
+    cy.get(':nth-child(2) > .nav-link').click()
+    cy.get('.navbar-brand').click()
+    cy.url().should('include', '/localhost')
+    cy.getCookie('your-session-cookie').should('exist')
+  })
+})
